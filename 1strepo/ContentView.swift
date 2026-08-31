e46408fac9754fa1e0fc5e33bec644b7a5fd7a08
Braden <lgybraden@gmail.com>
2026-08-31 17:11:21 +0800

    Testing

--- 1strepo/ContentView.swift
+++ 1strepo/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("DID U KNOW THE MOON IS NOT MAKES OF TREES  ")
+            
         }
         .padding()
     }
